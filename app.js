c71f2b618e1767ae1d0630d01a9556f340c17365
Rohan_Islam <rohanislam3669@gmail.com>
2024-05-28 11:27:26 +0600

    kl

--- app.js
+++ app.js
@@ -2,9 +2,22 @@ let userScore = 0;
 let compScore = 0;
 const choices = document.querySelectorAll(".choice");
 
+const genComChoice = () => {
+  const options = ["Rock", "Paper", "Scissors"];
+  const ranIdx = Math.floor(Math.random() * 3);
+  return options[ranIdx];
+};
+
+const playgame = (userChoice) => {
+  const comChoice = genComChoice();
+  if (userChoice === comChoice) {
+    console.log("match is draw");
+  }
+};
+
 choices.forEach((choice) => {
   choice.addEventListener("click", () => {
-    let choiceId = choice.getAttribute("id");
-    console.log(`choice was clicked ${choiceId}`);
+    let userChoice = choice.getAttribute("id");
+    playgame(userChoice);
   });
 });
